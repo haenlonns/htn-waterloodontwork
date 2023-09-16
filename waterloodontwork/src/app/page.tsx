23433460a408aea669a30e7bc94270bb820a60e9
daniel-su1 <107098486+daniel-su1@users.noreply.github.com>
2023-09-16 18:46:59 -0400

    add fade in animations

--- waterloodontwork/src/app/page.tsx
+++ waterloodontwork/src/app/page.tsx
@@ -5,6 +5,28 @@ import styles from "./page.module.css";
 import { useState } from "react";
 import Card from "./components/card";
 import { motion, AnimatePresence } from "framer-motion";
+import styled from "styled-components";
+
+const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: center; // Center the buttons
+  gap: 20px; // Gap between buttons
+  position: absolute;
+  bottom: 10%;
+  width: 100%;
+`;
+
+
+const SwipeButton = styled.button<{ color: string }>`
+  background-color: ${props => props.color};
+  border: none;
+  border-radius: 50%;
+  width: 50px;  // Adjusted size
+  height: 50px; // Adjusted size
+  cursor: pointer;
+  outline: none;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+`;
 
 
 const users = [
@@ -21,20 +43,31 @@ const users = [
 
 const Home = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [exitDirection, setExitDirection] = useState<number | undefined>();
 
   const onSwiped = (direction: string) => {
     if (direction === "Left" || direction === "Right") {
       setCurrentIndex((prevIndex) => prevIndex + 1);
     }
   };
+  const handleSwipe = (direction: "Left" | "Right") => {
+    setExitDirection(direction === "Left" ? -1 : 1);
+    setTimeout(() => {
+      onSwiped(direction);
+      setExitDirection(undefined);
+    }, 500);
+  };
 
   return (
     <div
       style={{
         display: "flex",
+        flexDirection: "column",
         justifyContent: "center",
         alignItems: "center",
-        height: "100vh", // This assumes the entire viewport height
+        height: "100vh",
+        position: "relative",
+        backgroundColor: "#f5f5f5",
       }}
     >
       {currentIndex < users.length && (
@@ -42,9 +75,19 @@ const Home = () => {
           key={currentIndex}
           name={users[currentIndex].name}
           image={users[currentIndex].image}
-          onSwiped={onSwiped}
+          onSwiped={handleSwipe}
+          exitDirection={exitDirection}
         />
       )}
+
+      <ButtonContainer>
+        <SwipeButton color="red" onClick={() => handleSwipe("Left")}>
+          ❌
+        </SwipeButton>
+        <SwipeButton color="green" onClick={() => handleSwipe("Right")}>
+          ❤️
+        </SwipeButton>
+      </ButtonContainer>
     </div>
   );
 };
